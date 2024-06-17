79137d1d7768f21cf69a6efadd6d09c93eff0e64
nbardavid <nbabardavid@gmail.com>
2024-06-17 13:40:27 +0200

    save

--- include/IRCServer.hpp
+++ include/IRCServer.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <poll.h>
 
 class IRCServer {
 public:
@@ -16,11 +17,20 @@ private:
     std::string password_;
     int serverSocket_;
     std::vector<int> clients_;
+    std::vector<struct pollfd> poll_fds_;
+    std::map<int, std::string> nicknames_; // Map pour stocker les pseudonymes des clients
+    std::map<int, std::string> usernames_; // Map pour stocker les noms d'utilisateurs des clients
+    std::map<int, std::string> channels_;  // Map pour stocker les canaux des clients
 
     void initializeSocket();
     void acceptConnections();
     void handleClient(int clientSocket);
     void closeSocket(int socket);
+    void broadcastMessage(int senderSocket, const std::string& message);
+
+    void handleNickCommand(int clientSocket, const std::string& nickname);
+    void handleUserCommand(int clientSocket, const std::string& userInfo);
+    void handleJoinCommand(int clientSocket, const std::string& channel);
 };
 
 #endif // IRCSERVER_HPP
